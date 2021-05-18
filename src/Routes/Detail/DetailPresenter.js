35cb95c18617a1649fabd899404c2926943a4f50
suhyeonglee <sounmind777@gmail.com>
2021-05-18 12:55:13 +0900

    ~ #6.9 DetailPresenter

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,119 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const DetailPresenter = ({ result, loading, error }) => "hey it's Detail";
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  padding: 50px;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  position: relative;
+  z-index: 1;
+  height: 100%;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  height: 100%;
+`;
+
+const Data = styled.div`
+  width: 70%;
+  margin-left: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 32px;
+  margin-bottom: 10px;
+`;
+
+const ItemContainer = styled.div`
+  margin: 20px 0;
+  display: flex;
+`;
+
+const Item = styled.div``;
+
+const Divider = styled.span`
+  margin: 0 10px;
+`;
+
+const Overview = styled.p`
+  font-size: 12px;
+  opacity: 0.7px;
+  line-height: 1.5;
+  width: 80%;
+`;
+
+const DetailPresenter = ({ result, loading, error }) =>
+  loading ? (
+    <Loader></Loader>
+  ) : (
+    <Container>
+      <Backdrop
+        bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+      ></Backdrop>
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original${result.poster_path}`
+              : require("../../assets/noPoster.jpg")
+          }
+        ></Cover>
+        <Data>
+          <Title>
+            {result.original_title
+              ? result.original_title
+              : result.original_name}
+          </Title>
+          <ItemContainer>
+            <Item>
+              {result.release_date
+                ? result.release_date.split("-")[0]
+                : result.first_air_date.split("-")[0]}
+            </Item>
+            <Divider>|</Divider>
+            <Item>
+              {result.runtime ? result.runtime : result.episode_run_time[0]} min
+            </Item>
+            <Divider>|</Divider>
+            <Item>
+              {result.genres &&
+                result.genres.map((genre, index) =>
+                  index === result.genres.length - 1
+                    ? genre.name
+                    : `${genre.name} / `
+                )}
+            </Item>
+          </ItemContainer>
+          <Overview>{result.overview}</Overview>
+        </Data>
+      </Content>
+    </Container>
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -7,7 +7,7 @@ import Message from "Components/Message";
 import Poster from "Components/Poster";
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 20px;
 `;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
